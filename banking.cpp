2d56d094658ef99e304f7125f8eeb43034537c1f
NajeebSultan <najeebsultan070@gmail.com>
2025-01-14 21:59:20 +0500

    Add switch statements for withdrawal option for provoking functions

--- banking.cpp
+++ banking.cpp
@@ -38,6 +38,108 @@ int main() {
     }
 
     displayMenu();
-    }
+do {
+        cin >> option;
+        switch (option) {
+            case 1: {
+                cout << "Transaction Statement\n";
+                int accountNumber;
+                cout << "Enter the account number: ";
+                cin >> accountNumber;
 
-   
+                if (accountNumber == 123456789) {
+                    int bankOption;
+                    cout << "Please select the bank:\n";
+                    cout << "1. HBL\n2. Meezan\n3. MCB\n4. Allied\n";
+                    cin >> bankOption;
+
+                    cout << "How much money do you want to transfer?\n";
+                    displayWithdrawalOptions();
+                    cin >> withdrawalOption;
+
+                    switch (withdrawalOption) {
+                        case 1:
+                            amount = 500;
+                            break;
+                        case 2:
+                            amount = 1000;
+                            break;
+                        case 3:
+                            amount = 2000;
+                            break;
+                        case 4:
+                            amount = 5000;
+                            break;
+                        case 5:
+                            cout << "Enter the amount to transfer: ";
+                            cin >> amount;
+                            break;
+                        default:
+                            cout << "Invalid option!\n";
+                            continue;
+                    }
+
+                    if (amount <= totalAmount) {
+                        totalAmount -= amount;
+                        cout << "You transferred Rs. " << amount << ".\nRemaining balance: Rs. " << totalAmount << "\n";
+                    } else {
+                        cout << "Not enough balance.\n";
+                    }
+                } else {
+                    cout << "Invalid account number.\n";
+                }
+                break;
+            }
+
+            case 2: {
+                displayWithdrawalOptions();
+                cin >> withdrawalOption;
+
+                switch (withdrawalOption) {
+                    case 1:
+                        amount = 500;
+                        break;
+                    case 2:
+                        amount = 1000;
+                        break;
+                    case 3:
+                        amount = 2000;
+                        break;
+                    case 4:
+                        amount = 5000;
+                        break;
+                    case 5:
+                        cout << "Enter the amount to withdraw: ";
+                        cin >> amount;
+                        break;
+                    default:
+                        cout << "Invalid option!\n";
+                        continue;
+                }
+
+                if (amount <= totalAmount) {
+                    totalAmount -= amount;
+                    cout << "You withdrew Rs. " << amount << ".\nRemaining balance: Rs. " << totalAmount << "\n";
+                } else {
+                    cout << "Not enough balance.\n";
+                }
+                break;
+            }
+
+            case 3:
+                cout << "Your total balance is: Rs. " << totalAmount << "\n";
+                break;
+
+            case 4:
+                cout << "Exiting. Thank you for using our service!\n";
+                break;
+
+            default:
+                cout << "Invalid option. Please try again.\n";
+                displayMenu();
+        }
+    } while (option != 4);
+
+    return 0;
+}
+  
